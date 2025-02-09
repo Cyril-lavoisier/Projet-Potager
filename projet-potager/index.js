4d9cd073e37523429269b760f512ebd028f4abfd
Cyril Lavoisier <cyril.lavoisier1@gmail.com>
2025-02-09 23:11:09 +0100

    Modification index?js et babel

--- projet-potager/index.js
+++ projet-potager/index.js
@@ -1,5 +1,10 @@
+import 'react-native-polyfill-globals/auto';
+import { Buffer } from 'buffer';
+import 'react-native-fetch-api';
+global.Buffer = Buffer;
 import { registerRootComponent } from 'expo';
 import App from './App';
+
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
