19a56c50f5f350d1dd7589411d3d685d5a39ef6b
Cyril Lavoisier <cyril.lavoisier1@gmail.com>
2025-02-12 21:40:23 +0100

    Ignorance de react native polyfill pour jest

--- projet-potager/jest.config.js
+++ projet-potager/jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
     preset: 'react-native',
     setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
     transformIgnorePatterns: [
-      'node_modules/(?!(react-native|@react-native|react-clone-referenced-element)/)',
+      'node_modules/(?!(react-native|@react-native|react-clone-referenced-element|react-native-fetch-api|react-native-polyfill-globals)/)',
     ],
   };
