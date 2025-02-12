677ed07cdf72c2631d6d6f229c37375401d621d4
Cyril Lavoisier <cyril.lavoisier1@gmail.com>
2025-02-12 23:53:59 +0100

    modification packageJson et babel

--- projet-potager/babel.config.js
+++ projet-potager/babel.config.js
@@ -4,5 +4,6 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     plugins: ['@babel/plugin-transform-runtime'],
     presets: ['module:metro-react-native-babel-preset'],
+    plugins: ['@babel/plugin-transform-private-methods'],
   };
 };
